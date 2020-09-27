9c3dadff0d2e205755745410da788b2fde5c3289
Deepak Joshi <Deepak.Joshi@wsp.com>
2020-09-27 13:03:16 +0530

    Added code in github

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DJBlogToDo.MvcApplication" Language="C#" %>
---
+++ Models/Todo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DJBlogToDo.Models
+{
+    public class Todo
+    {
+        public Todo()
+        {
+            this.Status = "Active";
+        }
+        public int ID { get; set; }
+        public string Description { get; set; }
+
+        [Display(Name = "Due Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DueDate { get; set; }
+        //Completed
+        //Active
+        //Pending
+        public string Status { get; set; }
+    }
+}
